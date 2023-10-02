0091d9418e6e31dfe785370ed66122c74c2b122f
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2023-10-02 22:57:11 +0530

    fix : add cpu support with offload

--- LawGPT.py
+++ LawGPT.py
@@ -1,5 +1,5 @@
 from langchain.vectorstores import Chroma
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM 
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoConfig 
 from transformers import pipeline
 import torch
 from langchain.llms import HuggingFacePipeline
@@ -9,7 +9,10 @@ import gradio as gr
 
 def chat(chat_history, user_input):
 
-    bot_response = qa_chain({"query": user_input})
+    template = f"""Act as a chatbot that solves legal queries and answer the below query of an undertrial prisoner in a calm manner.
+    Query : {user_input} """
+
+    bot_response = qa_chain({"query": template})
     bot_response = bot_response['result']
     response = ""
     for letter in ''.join(bot_response):
@@ -17,11 +20,18 @@ def chat(chat_history, user_input):
         yield chat_history + [(user_input, response)]
 
 checkpoint = "MBZUAI/LaMini-Flan-T5-783M"   
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint,cache_dir='./model_config')
 base_model = AutoModelForSeq2SeqLM.from_pretrained(
     checkpoint,
     device_map="auto",
+    offload_folder="offload",
+    offload_state_dict = True,
     torch_dtype = torch.float32)
+
+# config = AutoConfig.from_pretrained('./config.json')
+# model_state_dict = torch.load('./pytorch_model.bin')
+# base_model = AutoModelForSeq2SeqLM.from_config(config)
+# base_model.load_state_dict(model_state_dict)
 
 embeddings = SentenceTransformerEmbeddings(model_name="sentence-transformers/multi-qa-mpnet-base-dot-v1")
 
@@ -29,11 +39,11 @@ db = Chroma(persist_directory="ipc_vector_data", embedding_function=embeddings)
 
 pipe = pipeline(
     'text2text-generation',
+    tokenizer= tokenizer,
     model = base_model,
-    tokenizer = tokenizer,
     max_length = 512,
     do_sample = True,
-    temperature = 0.3,
+    temperature = 0.8,
     top_p= 0.95
 )
 local_llm = HuggingFacePipeline(pipeline=pipe)
@@ -57,13 +67,13 @@ with gr.Blocks() as gradioUI:
         with gr.Column():
             submit_btn = gr.Button("Submit", variant="primary")
         with gr.Column():
-            clear_input_btn = gr.Button("Clear Input")
+            clear_input_btn = gr.Button("Clear Input", variant='secondary')
         with gr.Column():
-            clear_chat_btn = gr.Button("Clear Chat")
+            clear_chat_btn = gr.Button("Clear Chat", variant='stop')
 
     submit_btn.click(chat, [chatbot, input_query], chatbot)
-    submit_btn.click(lambda: gr.update(value=""), None, input_query, queue=False)
+    submit_btn.click(lambda: gr.Textbox(value=""), None, input_query, queue=False)
     clear_input_btn.click(lambda: None, None, input_query, queue=False)
     clear_chat_btn.click(lambda: None, None, chatbot, queue=False)
 
-gradioUI.queue().launch()
+gradioUI.queue().launch(share=True)
